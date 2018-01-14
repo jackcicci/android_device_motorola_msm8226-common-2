LineageOS/android_device_motorola_msm8226-common

sensors: falcon-peregrine: Switch to REL events for the accel
The kernel now uses REL events for the accelerometer.

Change-Id: I2739e07a120e57686071b58ebd4ccefc2912cbf6

libsensors/falcon-peregrine/sensors.h
@@ -53,9 +53,9 @@ __BEGIN_DECLS
 
 #define MAG_MIN_DELAY_NS            200000000LL /* nanoseconds */
 
-#define EVENT_TYPE_ACCEL_X          ABS_Y
-#define EVENT_TYPE_ACCEL_Y          ABS_X
-#define EVENT_TYPE_ACCEL_Z          ABS_Z
+#define EVENT_TYPE_ACCEL_X          REL_X
+#define EVENT_TYPE_ACCEL_Y          REL_Y
+#define EVENT_TYPE_ACCEL_Z          REL_Z
 
 #define EVENT_TYPE_SO               MSC_RAW
 

jackcicci/android_device_motorola_msm8226-common-2

libsensors/falcon-peregrine/sensors.h
@@ -53,9 +53,9 @@ __BEGIN_DECLS
 
 #define MAG_MIN_DELAY_NS            200000000LL /* nanoseconds */
 
-#define EVENT_TYPE_ACCEL_X          ABS_Y
-#define EVENT_TYPE_ACCEL_Y          ABS_X
-#define EVENT_TYPE_ACCEL_Z          ABS_Z
+#define EVENT_TYPE_ACCEL_X          REL_Y
+#define EVENT_TYPE_ACCEL_Y          REL_X
+#define EVENT_TYPE_ACCEL_Z          REL_Z
 
 #define EVENT_TYPE_SO               MSC_RAW
 